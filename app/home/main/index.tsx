faa3632c3a15a396ea39d6df6157f9496801dca9
macha1316 <hiromacha1116@gmail.com>
2025-02-01 17:28:42 +0900

    add: fabの追加

--- app/home/main/index.tsx
+++ app/home/main/index.tsx
@@ -1,3 +1,4 @@
+import Button from "@/src/components/presenter/Button";
 import Table from "@/src/components/presenter/Table";
 import { ScrollView, StyleSheet, View } from "react-native";
 
@@ -7,12 +8,16 @@ export default function Index() {
       <ScrollView contentContainerStyle={styles.scrollContent}>
         <Table />
         <Table />
-        {/* <Table />
-        <Table />
-        <Table />
-        <Table />
-        <Table /> */}
       </ScrollView>
+
+      <View style={styles.fabContainer}>
+        <Button
+          text="+"
+          textStyle={styles.fabText}
+          onPress={() => {}}
+          style={styles.fabButton}
+        />
+      </View>
     </View>
   );
 }
@@ -25,5 +30,29 @@ const styles = StyleSheet.create({
   scrollContent: {
     padding: 20,
     gap: 10,
+  },
+  fabContainer: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+  },
+  // TODO: fabButtonが他のところで必要になったら共通化
+  fabButton: {
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    backgroundColor: "#007AFF",
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  fabText: {
+    color: "#fff",
+    fontSize: 30,
+    lineHeight: 34,
   },
 });
