d9602e54a62981c8c1a7bfcd71d2b8f992b45a68
macha1316 <hiromacha1116@gmail.com>
2025-02-09 14:02:18 +0900

    add: handlePanResponderRelease時に処理できるように修正

--- app/note/index.tsx
+++ app/note/index.tsx
@@ -1,12 +1,5 @@
-import { useEffect, useRef, useState } from "react";
-import {
-  LayoutChangeEvent,
-  PanResponder,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import useNoteContainer from "@/src/container/useNoteContainer";
+import { StyleSheet, Text, View } from "react-native";
 
 const testData = `
   The sky was an endless stretch of gray, 
@@ -20,108 +13,8 @@ const testData = `
 `;
 
 export default function Note() {
-  const words = [...testData.matchAll(/[\w'-]+|[.,!?;]|\n+/g)].map(
-    (match, index, array) => ({
-      id: index,
-      text: match[0],
-      start: match.index,
-      end: match.index + match[0].length - 1,
-      type: match[0].includes("\n") ? "newline" : "word",
-      newLineCount: match[0].length,
-      isBeforePunctuation:
-        index < array.length - 1 && /[.,!?;]/.test(array[index + 1][0]),
-    })
-  );
-
-  const [selectedIndices, setSelectedIndices] = useState<number[]>([]);
-  const wordPositions = useRef<
-    { id: number; x: number; y: number; width: number; height: number }[]
-  >([]);
-
-  const startIndexRef = useRef<number | null>(null);
-  const endIndexRef = useRef<number | null>(null);
-
-  // 各単語の座標を保存
-  const onWordLayout = (event: LayoutChangeEvent, id: number) => {
-    const { x, y, width, height } = event.nativeEvent.layout;
-    wordPositions.current = [
-      // 違うidの単語の座標はそのまま
-      ...wordPositions.current.filter((w) => w.id !== id),
-      { id, x, y, width, height },
-    ];
-  };
-
-  useEffect(() => {
-    console.log("🟢 useEffect - selectedIndices changed:", selectedIndices);
-  }, [selectedIndices]);
-
-  // タッチイベント
-  const panResponder = useRef(
-    PanResponder.create({
-      // イベントを受け取るかどうかの判定
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderGrant: (event, gestureState) => {
-        // gestureState === ドラッグ開始位置
-        const startIdx = getClosestWordIndex(gestureState.x0, gestureState.y0);
-        startIndexRef.current = startIdx;
-        endIndexRef.current = startIdx;
-        setSelectedIndices([startIdx]);
-      },
-      onPanResponderMove: (event, gestureState) => {
-        const endIdx = getClosestWordIndex(
-          gestureState.moveX,
-          gestureState.moveY
-        );
-        endIndexRef.current = endIdx;
-        updateSelectedRange();
-      },
-      onPanResponderRelease: () => {
-        startIndexRef.current = null;
-        endIndexRef.current = null;
-      },
-    })
-  ).current;
-
-  const insets = useSafeAreaInsets();
-
-  // タッチ位置を調節して最も近い単語を探す
-  const getClosestWordIndex = (x: number, y: number): number => {
-    // 端末によって調整値変える必要あるかも(マジックナンバーやめる)
-    const adjustedY = y - (insets.top + 55);
-    const adjustedX = x - 20;
-    let closestIndex = 0;
-    let minDistance = Infinity;
-
-    wordPositions.current.forEach((word) => {
-      // 単語の中心点を計算
-      const centerX = word.x + word.width / 2;
-      const centerY = word.y + word.height / 2;
-
-      // タッチ位置と単語の中心点の距離を計算
-      const distance = Math.sqrt(
-        (centerX - adjustedX) ** 2 + (centerY - adjustedY) ** 2
-      );
-
-      if (distance < minDistance) {
-        minDistance = distance;
-        closestIndex = word.id;
-      }
-    });
-
-    return closestIndex;
-  };
-
-  const updateSelectedRange = () => {
-    if (startIndexRef.current !== null && endIndexRef.current !== null) {
-      // ドラッグの方向によってmin, maxを決める
-      const minIdx = Math.min(startIndexRef.current, endIndexRef.current);
-      const maxIdx = Math.max(startIndexRef.current, endIndexRef.current);
-
-      setSelectedIndices(
-        Array.from({ length: maxIdx - minIdx + 1 }, (_, i) => minIdx + i)
-      );
-    }
-  };
+  const { words, selectedIndices, onWordLayout, panResponder } =
+    useNoteContainer({ testData });
 
   return (
     <View style={styles.container} {...panResponder.panHandlers}>
--- src/container/useNoteContainer.ts
+++ src/container/useNoteContainer.ts
@@ -1,7 +1,149 @@
+import { useCallback, useMemo, useRef, useState } from "react";
+import {
+  GestureResponderEvent,
+  LayoutChangeEvent,
+  PanResponder,
+  PanResponderGestureState,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
 type NoteContainerProps = {
   testData: string;
 };
 
 export default function useNoteContainer({ testData }: NoteContainerProps) {
-  return {};
+  const insets = useSafeAreaInsets();
+
+  const wordPositions = useRef<
+    { id: number; x: number; y: number; width: number; height: number }[]
+  >([]);
+
+  const startIndexRef = useRef<number | null>(null);
+  const endIndexRef = useRef<number | null>(null);
+  const [selectedIndices, setSelectedIndices] = useState<number[]>([]);
+
+  const words = useMemo(() => {
+    return Array.from(testData.matchAll(/[\w'-]+|[.,!?;]|\n+/g)).map(
+      (match, index, array) => {
+        const text = match[0];
+        const start = match.index ?? 0;
+        const end = start + text.length - 1;
+
+        return {
+          id: index,
+          text,
+          start,
+          end,
+          type: text.includes("\n") ? "newline" : "word",
+          newLineCount: text.length,
+          isBeforePunctuation:
+            !!array[index + 1] && /[.,!?;]/.test(array[index + 1][0]),
+        };
+      }
+    );
+  }, [testData]);
+
+  // 各単語の座標を保存
+  const onWordLayout = (event: LayoutChangeEvent, id: number) => {
+    const { x, y, width, height } = event.nativeEvent.layout;
+    wordPositions.current = [
+      ...wordPositions.current.filter((w) => w.id !== id),
+      { id, x, y, width, height },
+    ];
+  };
+
+  // タッチ位置を調節して最も近い単語を探す
+  const getClosestWordIndex = useCallback(
+    (x: number, y: number) => {
+      // 端末によって調整値変える必要あるかも(マジックナンバーやめる)
+      const adjustedY = y - (insets.top + 55);
+      const adjustedX = x - 20;
+
+      const { id: closestIndex } = wordPositions.current.reduce(
+        (closest, word) => {
+          // 単語の中心点を計算
+          const centerX = word.x + word.width / 2;
+          const centerY = word.y + word.height / 2;
+
+          // タッチ位置と単語の中心点の距離を計算
+          const distance = Math.sqrt(
+            (centerX - adjustedX) ** 2 + (centerY - adjustedY) ** 2
+          );
+
+          return distance < closest.distance
+            ? { id: word.id, distance }
+            : closest;
+        },
+        { id: 0, distance: Infinity }
+      );
+
+      return closestIndex;
+    },
+    [insets.top]
+  );
+
+  const updateSelectedRange = useCallback(() => {
+    if (startIndexRef.current !== null && endIndexRef.current !== null) {
+      const minIdx = Math.min(startIndexRef.current, endIndexRef.current);
+      const maxIdx = Math.max(startIndexRef.current, endIndexRef.current);
+
+      const oldMin = selectedIndices.at(0);
+      const oldMax = selectedIndices.at(-1);
+      const currentMin = minIdx;
+      const currentMax = maxIdx;
+
+      if (oldMin === currentMin && oldMax === currentMax) {
+        return;
+      }
+
+      setSelectedIndices((prev) => {
+        return Array.from(
+          { length: maxIdx - minIdx + 1 },
+          (_, i) => minIdx + i
+        );
+      });
+    }
+  }, [selectedIndices]);
+
+  const handlePanResponderGrant = useCallback(
+    (event: GestureResponderEvent, gestureState: PanResponderGestureState) => {
+      const startIdx = getClosestWordIndex(gestureState.x0, gestureState.y0);
+      startIndexRef.current = startIdx;
+      endIndexRef.current = startIdx;
+      setSelectedIndices([startIdx]);
+    },
+    [getClosestWordIndex]
+  );
+
+  const handlePanResponderMove = useCallback(
+    (event: GestureResponderEvent, gestureState: PanResponderGestureState) => {
+      const endIdx = getClosestWordIndex(
+        gestureState.moveX,
+        gestureState.moveY
+      );
+
+      endIndexRef.current = endIdx;
+
+      updateSelectedRange();
+    },
+    [getClosestWordIndex, updateSelectedRange]
+  );
+
+  const handlePanResponderRelease = useCallback(() => {
+    startIndexRef.current = null;
+    endIndexRef.current = null;
+  }, [selectedIndices]);
+
+  const panResponder = useMemo(
+    () =>
+      PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: handlePanResponderGrant,
+        onPanResponderMove: handlePanResponderMove,
+        onPanResponderRelease: handlePanResponderRelease,
+      }),
+    [handlePanResponderGrant, handlePanResponderMove, handlePanResponderRelease]
+  );
+
+  return { words, selectedIndices, onWordLayout, panResponder };
 }
