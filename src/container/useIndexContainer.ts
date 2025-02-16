f0834b9ae28d1eca52e411ac028f2d2bd1ca0f3e
macha1316 <hiromacha1116@gmail.com>
2025-02-16 20:13:43 +0900

    add: firebaseの自動ログイン

--- src/container/useIndexContainer.ts
+++ src/container/useIndexContainer.ts
@@ -1,4 +1,6 @@
+import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
+import { auth } from "../firebase/config/firebase";
 import useNavigation from "../hooks/useNavigation";
 
 // のちにAPI経由でログイン状態を取得する処理の追加
@@ -9,10 +11,30 @@ export default function useIndexContainer() {
   const { pageReplace } = useNavigation();
 
   useEffect(() => {
+    // Firebase のログイン状態を監視
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("User is logged in:", user.uid);
+        setIsLoggedIn(true);
+        pageReplace("/home/main");
+        setIsLoading(true);
+      } else {
+        console.log("User is NOT logged in");
+        setIsLoggedIn(false);
+        pageReplace("/auth/login");
+        setIsLoading(true);
+      }
+      setIsLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
     // requestAnimationFrameを使って、画面遷移を遅延させる(最適でないかも)
     requestAnimationFrame(() => {
-      if (!isLoading) {
-        if (!isLoggedIn) {
+      if (isLoading) {
+        if (isLoggedIn) {
           pageReplace("/home/main");
         } else {
           pageReplace("/auth/login");
